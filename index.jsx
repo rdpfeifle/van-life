61a75e4af485a995f89732bcc1e85db8b00cfbdb
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-08-16 11:40:30 -0600

    Add index.jsx file

---
+++ index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+function Vans() {
+  return (
+    <h1>I am the Vans page</h1>
+  )
+}
+ 
+function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/" className="logo">#VanLife</Link>
+        <div className="links-container">
+          <Link to="/about" className="nav-link">About</Link>
+          <Link to="/vans" className="nav-link">Vans</Link>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<App />);
