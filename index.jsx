3d17fff011c964364185b8edef7b25dcec624522
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-08-20 08:55:53 -0600

    Import server and Vans component

--- index.jsx
+++ index.jsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Vans from "./pages/Vans";
 
-function Vans() {
-  return (
-    <h1>I am the Vans page</h1>
-  )
-}
- 
+import "./server";
+
 function App() {
   return (
     <BrowserRouter>
       <nav>
-        <Link to="/" className="logo">#VanLife</Link>
+        <Link to="/" className="logo">
+          #VanLife
+        </Link>
         <div className="links-container">
-          <Link to="/about" className="nav-link">About</Link>
-          <Link to="/vans" className="nav-link">Vans</Link>
+          <Link to="/about" className="nav-link">
+            About
+          </Link>
+          <Link to="/vans" className="nav-link">
+            Vans
+          </Link>
         </div>
       </nav>
       <Routes>
@@ -26,9 +29,7 @@ function App() {
         <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-ReactDOM
-  .createRoot(document.getElementById('root'))
-  .render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
