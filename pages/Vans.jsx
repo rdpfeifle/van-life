9b82a2f60956c539a1954bdfade7e30122ffe8e2
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-08-20 09:07:05 -0600

    Create Vans component and style its elements

---
+++ pages/Vans.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+export default function Vans() {
+  const [vans, setVans] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/vans")
+      .then((response) => response.json())
+      .then((data) => setVans(data.vans));
+  }, []);
+
+  // id, name, price, description, imageUrl, type properties
+  const vansElements = vans.map((van) => (
+    <div key={van.id} className="van-tile">
+      <img src={van.imageUrl} />
+      <div className="van-info">
+        <h3>{van.name}</h3>
+        <p>
+          ${van.price}
+          <span>/day</span>
+        </p>
+      </div>
+      <i className={`van-type ${van.type} selected`}>{van.type}</i>
+    </div>
+  ));
+
+  return (
+    <div className="van-list-container">
+      <h1>Explore our van options</h1>
+      <div className="van-list">{vansElements}</div>
+    </div>
+  );
+}
