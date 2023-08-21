1e45c347204ce9f6fb7c01b5a92bda2ec66e0cf3
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-08-21 09:17:11 -0600

    Create the feature to display van info

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -1,3 +1,34 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 export default function VanDetail() {
-  return <h1>I am the van detail page</h1>;
+  const params = useParams();
+
+  const [van, setVan] = useState(null);
+
+  useEffect(() => {
+    fetch(`/api/vans/${params.id}`)
+      .then((response) => response.json())
+      .then((data) => setVan(data.vans));
+  }, [params.id]);
+
+  return (
+    <div className="van-detail-container">
+      {van ? (
+        <div className="van-detail">
+          <img src={van.imageUrl} alt={`Van that is ${van.name}`} />
+          <i className={`van-type ${van.type} selected`}>{van.type}</i>
+          <h2>{van.name}</h2>
+          <p className="van-price">
+            ${van.price}
+            <span>/day</span>
+          </p>
+          <p>{van.description}</p>
+          <button className="rental-btn">Rent this van</button>
+        </div>
+      ) : (
+        <h2>Loading...</h2>
+      )}
+    </div>
+  );
 }
